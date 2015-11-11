f45bdf8285e50e378b896a90792014de50b0b5c3
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2015-11-11 20:49:55 +0530

    Implement onNavBack handler for back navigation
     - Implement onNavBack in BaseController so that it is not duplicated in
       controllers for each page that has back navigation
    
     - If there is a previous hash in history, app goes to this previous hash.
       Else app goes to home view
    
     - Add back navigation button to Not Found page

--- WebContent/controller/BaseController.js
+++ WebContent/controller/BaseController.js
@@ -1,7 +1,25 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(Controller){
-	"use strict";
-	
-	return Controller.extend("sap.ui.demo.nav.controller.BaseController",{
-		
-	});
-});
+sap.ui.define([ "sap/ui/core/mvc/Controller", "sap/ui/core/routing/History" ],
+		function(Controller, History) {
+			"use strict";
+
+			return Controller.extend(
+					"sap.ui.demo.nav.controller.BaseController", {
+						
+						getRouter: function(){
+							return sap.ui.core.UIComponent.getRouterFor(this);
+						},
+						
+						onNavBack: function(){
+							var oHistory, sPreviousHash;
+							oHistory = History.getInstance();
+							sPreviousHash = oHistory.getPreviousHash();
+							
+							if(sPreviousHash !== undefined){
+								window.history.go(-1);
+							} else {
+								this.getRouter().navTo("appHome",{},true);
+							}
+						}
+
+					});
+		});
