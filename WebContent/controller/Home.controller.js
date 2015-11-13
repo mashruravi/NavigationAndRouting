e1cbc735acafa804ea04cb7ea1c46eba764cfada
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2015-11-13 14:28:05 +0530

    Navigate to a Route with a Hard-Coded Pattern
     - Add button to Home page taking user to list of employees
    
     - Add employees route with corresponding target to manifest.json
    
     - Display list of employees in EmployeeList view

--- WebContent/controller/Home.controller.js
+++ WebContent/controller/Home.controller.js
@@ -1,14 +1,19 @@
-sap.ui.define([ "sap/ui/demo/nav/controller/BaseController" ], function(BaseController) {
+sap.ui.define([ "sap/ui/demo/nav/controller/BaseController" ], function(
+		BaseController) {
 
 	"use strict";
-	
+
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
 
 		onDisplayNotFound : function(oEvent) {
 			// Display the "not found" target without changing the hash
 			this.getRouter().getTargets().display("notFound", {
-				fromTarget: "home"
+				fromTarget : "home"
 			});
+		},
+
+		onNavToEmployees : function() {
+			this.getRouter().navTo("employeeList");
 		}
 	});
 
